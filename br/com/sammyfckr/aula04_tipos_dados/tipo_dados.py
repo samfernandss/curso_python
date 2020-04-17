168a69f4f54ba8947fe282456ba2b7c7d71ea911
sammyfckr <samwallflowergirl@gmail.com>
2020-04-17 19:07:09 -0300

    Aulas da seção 4

---
+++ br/com/sammyfckr/aula04_tipos_dados/tipo_dados.py
@@ -0,0 +1,30 @@
+#int
+num = 1_000_000
+print(num)
+
+#float
+valor = 1.4
+
+#boolean (inicial maiúscula sempre)
+ativo = True
+print(not ativo)
+
+#string
+"isso é"
+'isso é'
+'''isso é'''
+"""isso é. (acho)"""
+
+nome = "Sam Fernandes"
+print(nome.upper())
+print(nome[0:3])
+#nome[::-1] -> Do primeiro ao último elemento e inverter (inversão de string)
+
+#escopo
+"""
+Não é tipada
+"""
+
+
+
+
