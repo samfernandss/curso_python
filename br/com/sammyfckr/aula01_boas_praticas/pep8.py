8261aa0b8a1834922197cc8b7850cb43e61c22d4
sammyfckr <samwallflowergirl@gmail.com>
2020-04-17 16:18:28 -0300

    Meu segundo commit em Python!

---
+++ br/com/sammyfckr/aula01_boas_praticas/pep8.py
@@ -0,0 +1,11 @@
+"""
+PEP8
+1- Inicial maiuscula ou Camel Case para classe
+2- Nome minusculo ou separado por underline pra função e variável
+3- 4 espaços para identação (não usar tab)
+4- Linhas em branco (pelo menos duas antes das classes e uma entre funções ou variáveis)
+5- Imports em linhas separadas, no topo depois de de comenários
+6- Espaços em expressões e instruções (não meter em tudo)
+7- Termine instruções com uma nova linha
+
+"""
