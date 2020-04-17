9b06d8a00d84a82efdc867182b309aa85f961dc7
sammyfckr <samwallflowergirl@gmail.com>
2020-04-17 14:01:08 -0300

    Meu segundo commit em Python!

--- br/com/sammyfckr/aula01_boas_praticas/teste.py
+++ br/com/sammyfckr/aula01_boas_praticas/teste.py
@@ -1,1 +1,8 @@
-print("hello world!")
+nome = "Sam"
+idade = 17
+if idade < 18:
+    print("Epa! " + nome + " voce não pode entrar!")
+else:
+    print("OK " + nome + ", voce pode entrar!")
+
+print("teste!")
