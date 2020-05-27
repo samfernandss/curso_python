eff2e2d3fb22e5077df916c896711c53c40a82eb
sammyfckr <samwallflowergirl@gmail.com>
2020-05-27 17:06:57 -0300

    loop and range

--- br/com/sammyfckr/aula07_estruturas_repeticao/loop_for.py
+++ br/com/sammyfckr/aula07_estruturas_repeticao/loop_for.py
@@ -1,14 +1,18 @@
-nome = 'Sam Fernandes'
+'''nome = 'Sam Fernandes'
 for letra in nome:
     print(letra)
 
 for numero in range(1, 10):
-    print(numero*2)
+    print(numero*2)'''
 
 qnt = int(input("quanto?"))
 for numero in range(1, qnt+1):
     print(f'qntdade {numero}')
 
-for num in range(1,11):
-    print('\U0001F645' * num)
+numbers = [1, 2, 3]
+for num in numbers:
+    print(f'olha aí {num}')
+
+'''for num in range(1,11):
+    print('\U0001F645' * num)'''
 
